9ad56bd7ca08ef6a11d35a7c0b4b6b1783ab4ae7
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-02-18 16:01:28 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""
-Finding hot top 10 post
-"""
+"""DOCS"""
 import requests
 
 
 def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Myapi-app'}
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
 
-    r = requests.get(url+'?limit=10', headers=headers)
-    if r.status_code == 200:
-        value = r.json()
-        datas = value['data']['children']
-        for each in datas:
-            title = each['data']['title']
-            print(title)
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
     else:
-        print('None')
+        print(None)
