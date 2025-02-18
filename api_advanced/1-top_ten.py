120344503c61485cf3f41dc7fa04142a98e6caad
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-02-18 16:11:49 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""DOCS"""
+"""Module to fetch and print the top ten hot posts from a subreddit."""
 import requests
 
-
 def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    """Fetch and print the titles of the top 10 hot posts in a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts[:10]:
             print(post['data']['title'])
     else:
         print(None)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
