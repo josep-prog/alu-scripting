95ad4bbb77ea7704a2e016d718a967b728cd2699
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-02-18 16:17:14 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""Module to fetch and print the top ten hot posts from a subreddit."""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
+
 def top_ten(subreddit):
-    """Fetch and print the titles of the top 10 hot posts in a subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts[:10]:
-            print(post['data']['title'])
-    else:
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
         print(None)
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
