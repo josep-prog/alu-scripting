b441cd844e32ae8ea208d919b0ff6c98c9e8cacd
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-02-21 19:21:06 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,12 +2,9 @@
 """DOCS"""
 import requests
 
-
 def top_ten(subreddit):
     """Fetch and print the top 10 hot post titles of a subreddit."""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(
-        subreddit
-    )
+    reddit_url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers, allow_redirects=False)
 
