adef302feb34769849220cfbfccc23283d1156d9
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-02-21 17:43:28 +0200

    1-main.py

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -4,20 +4,9 @@
 """
 import sys
 
-def main():
-    """
-    Main function to test the top_ten function.
-    """
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
-        sys.exit(1)  # Exit with a non-zero status to indicate an error
-
-    try:
-        top_ten = __import__('1-top_ten').top_ten
+    else:
         top_ten(sys.argv[1])
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        sys.exit(1)  # Exit with a non-zero status to indicate an error
-
-if __name__ == '__main__':
-    main()
